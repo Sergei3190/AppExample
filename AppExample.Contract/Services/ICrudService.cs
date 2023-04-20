13756c373252f09b296913cb463163dd49fa22f6
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 12:35:33 +0300

    добавлены контракты для работы с сущностями БД

---
+++ AppExample.Contract/Services/ICrudService.cs
@@ -0,0 +1,12 @@
+﻿using AppExample.Contract.Dto.Base;
+
+namespace AppExample.Contract.Services;
+
+public interface ICrudService<T> where T : BaseDto
+{
+    Task<IEnumerable<T>> GetListAsync();
+    Task<T?> GetAsync(Guid id);
+    Task<Guid> CreateAsync(T? dto);
+    Task<Guid> UpdateAsync(T? dto);
+    Task<bool> DeleteAsync(Guid id);
+}
---
+++ AppExample.Contract/Services/ISaveService.cs
@@ -0,0 +1,6 @@
+﻿namespace AppExample.Contract.Services;
+
+public interface ISaveService<T> where T : class
+{
+    Task<int> SaveAsync(T context, CancellationToken token = default);
+}
