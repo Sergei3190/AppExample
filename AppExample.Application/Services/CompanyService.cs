ded0e7e4e09e0674e138575036f1cc473478e50d
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 12:36:51 +0300

    добавлены каскады сервисов работы с сущностями БД

---
+++ AppExample.Application/Services/CompanyService.cs
@@ -0,0 +1,45 @@
+﻿using AppExample.Contract.Dto;
+using AppExample.Contract.Services;
+using AppExample.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Logging;
+
+namespace AppExample.Application.Services;
+
+public class CompanyService : ICompanyService
+{
+    private readonly IDbContextFactory<AppExampleDb> _dbContexFactory;
+    private readonly ILogger<CompanyService> _logger;
+
+    public CompanyService(IDbContextFactory<AppExampleDb> dbContextFactory, ILogger<CompanyService> logger)
+    {
+        _dbContexFactory = dbContextFactory;    
+        _logger = logger;
+    }
+
+    public Task<Guid> CreateAsync(CompanyDto? dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CompanyDto?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<CompanyDto>> GetListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Guid> UpdateAsync(CompanyDto? dto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AppExample.Application/Services/EmployeeService.cs
@@ -0,0 +1,45 @@
+﻿using AppExample.Contract.Dto;
+using AppExample.Contract.Services;
+using AppExample.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Logging;
+
+namespace AppExample.Application.Services;
+
+public class EmployeeService : IEmployeeService
+{
+    private readonly IDbContextFactory<AppExampleDb> _dbContexFactory;
+    private readonly ILogger<EmployeeService> _logger;
+
+    public EmployeeService(IDbContextFactory<AppExampleDb> dbContextFactory, ILogger<EmployeeService> logger)
+    {
+        _dbContexFactory = dbContextFactory;
+        _logger = logger;
+    }
+
+    public Task<Guid> CreateAsync(EmployeeDto? dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EmployeeDto?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<EmployeeDto>> GetListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Guid> UpdateAsync(EmployeeDto? dto)
+    {
+        throw new NotImplementedException();
+    }
+}
