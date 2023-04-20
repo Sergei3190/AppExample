62b79e147239abff96519d769bc0f3779a5fb8d5
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 10:42:28 +0300

    добавлена настройка конфигурации приложения

--- AppExample/Program.cs
+++ AppExample/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+using AppExample.Configuration;
+using AppExample.DAL.Context;
+using AppExample.Extrenions;
+using AppExample.HostedServices;
+using AppExample.HostedServices.Settings;
+using AppExample.Inizializers;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+(IConfiguration config, string environment) congfiguration;
+
+if (!Debugger.IsAttached)
+{
+    Console.WriteLine("Работаем как служба.");
+    congfiguration = Config.GetConfiguration(argsArray: null, isService: true);
+}
+else
+{
+    Console.WriteLine("Работаем интерактивно.");
+    var configInizializer = new ConfigInitializer();
+    congfiguration = configInizializer.GetConfiguration();
+}
+
+if (congfiguration.config is null || congfiguration.environment is null)
+    return;
+
+var builder = Host.CreateDefaultBuilder()
+    .UseEnvironment(congfiguration.environment)
+    .ConfigureHostConfiguration(builder =>
+    {
+        builder.AddConfiguration(congfiguration.config);
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddDbContextFactory<AppExampleDb>(opt =>
+        {
+            opt.UseSqlServer(congfiguration.config.GetConnectionString("SqlServer"));
+        });
+        services.AddTransientServiceFactory();
+        services.AddScopedServiceFactory();
+        services.AddSingletonServiceFactory();
+
+        services.AddHostedService<AppHostedService>();
+        services.AddOptions<AppHostSettings>().Bind(hostContext.Configuration.GetSection("DbInitialSettings"));
+    })
+    .Build();
+
+await builder.RunAsync();
