2ba39bb84efc7ab80e3266f20434729d66c83d00
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 10:38:53 +0300

    добавлена библиотека приложения

---
+++ AppExample.Application/Data/TestData.cs
@@ -0,0 +1,21 @@
+﻿using AppExample.Domain.Entities.Public;
+
+namespace AppExample.Data;
+
+public static class TestData
+{
+    public static IEnumerable<Company> Companies { get; } = new List<Company>()
+    {
+        new () { Name = "Apple" },
+        new () { Name = "Google" },
+    };
+
+    public static IEnumerable<Employee> Employees { get; } = new List<Employee>()
+	{
+		new () { Age = 25, LastName = "Иванов", FirstName = "Иван", MiddleName = "Иванович", Company = Companies!.First() },
+		new () { Age = 27, LastName = "Петров", FirstName = "Петр", MiddleName = "Петрович", Company = Companies!.Last() },
+		new () { Age = 18, LastName = "Сидоров", FirstName = "Сидор", MiddleName = "Сидорович", Company = Companies!.Last() },
+		new () { Age = 19, LastName = "Короваев", FirstName = "Андрей", MiddleName = "Михалыч", Company = Companies!.First() },
+		new () { Age = 48, LastName = "Петьков", FirstName = "Ваня", MiddleName = "Сидорович", Company = Companies!.Last() }
+	};
+}
---
+++ AppExample.Application/Services/DbInitializerService.cs
@@ -0,0 +1,77 @@
+﻿using AppExample.Contract.Services;
+using AppExample.DAL.Context;
+using AppExample.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AppExample.Application.Services;
+
+public class DbInitializerService : IDbInitializerService
+{
+    private readonly IDbContextFactory<AppExampleDb> _dbContextFactory;
+    private readonly ILogger<DbInitializerService> _logger;
+
+    public DbInitializerService(
+        ILogger<DbInitializerService> logger, IDbContextFactory<AppExampleDb> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+        _logger = logger;
+    }
+
+    public async Task InitializeAsync(bool canRemove, bool canAddTestData, CancellationToken cancel = default)
+    {
+        if (canRemove)
+            await RemoveAsync(cancel).ConfigureAwait(false);
+
+        await using var _db = await _dbContextFactory.CreateDbContextAsync(cancel).ConfigureAwait(false);
+
+        _logger.LogInformation("Инициализация БД...");
+
+        _logger.LogInformation("Применение миграций БД...");
+        await _db.Database.MigrateAsync(cancel).ConfigureAwait(false);
+        _logger.LogInformation("Применение миграций БД выполнено");
+
+        if (canAddTestData)
+            await InitializerDataAsync(cancel).ConfigureAwait(false);
+
+        _logger.LogInformation("Инициализация БД выполнена успешно");
+    }
+
+    private async Task<bool> RemoveAsync(CancellationToken cancel = default)
+    {
+        await using var _db = await _dbContextFactory.CreateDbContextAsync(cancel).ConfigureAwait(false);
+
+        _logger.LogInformation("Удаление БД...");
+
+        var result = await _db.Database.EnsureDeletedAsync(cancel).ConfigureAwait(false);
+
+        _logger.LogInformation("{0}", result ? "Удаление БД выполнено успешно" : "Удаление БД не выполнено");
+
+        return result;
+    }
+
+    private async Task InitializerDataAsync(CancellationToken cancel)
+    {
+        await using var _db = await _dbContextFactory.CreateDbContextAsync(cancel).ConfigureAwait(false);
+
+        _logger.LogInformation("Инициализация БД тестовыми данными...");
+
+        if (await _db.Employees.AnyAsync(cancel).ConfigureAwait(false))
+        {
+            _logger.LogInformation("Инициализация БД тестовыми данными не требуется");
+            return;
+        }
+
+        await using var transaction = await _db.Database.BeginTransactionAsync(cancel).ConfigureAwait(false);
+
+        await _db.Companies.AddRangeAsync(TestData.Companies, cancel).ConfigureAwait(false);
+        await _db.Employees.AddRangeAsync(TestData.Employees, cancel).ConfigureAwait(false);
+
+        await _db.SaveChangesAsync(cancel).ConfigureAwait(false);
+
+        await transaction.CommitAsync().ConfigureAwait(false);
+
+        _logger.LogInformation("Инициализация БД тестовыми данными выполнена успешно");
+    }
+}
