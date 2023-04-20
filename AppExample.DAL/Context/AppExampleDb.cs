79acfde230f97065eb6699a13a0b3b8e609027fc
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 10:36:45 +0300

    добавлена библиотека контекста БД

---
+++ AppExample.DAL/Context/AppExampleDb.cs
@@ -0,0 +1,35 @@
+﻿using AppExample.Domain.Entities.Public;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AppExample.DAL.Context;
+
+public class AppExampleDb : DbContext
+{
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    public AppExampleDb()
+    {
+        
+    }
+
+    public AppExampleDb(DbContextOptions<AppExampleDb> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        Map(modelBuilder);
+    }
+
+    private static void Map(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new Company.Map());
+        builder.ApplyConfiguration(new Employee.Map());
+    }
+}
---
+++ AppExample.DAL/Migrations/20230419122534_UpdateTables.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppExample.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_employess_companies_CompanyId",
+                schema: "app",
+                table: "employess");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                schema: "app",
+                table: "employess",
+                newName: "company_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_employess_CompanyId",
+                schema: "app",
+                table: "employess",
+                newName: "IX_employess_company_id");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                schema: "app",
+                table: "companies",
+                newName: "name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_companies_Name",
+                schema: "app",
+                table: "companies",
+                newName: "IX_companies_name");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_employess_companies_company_id",
+                schema: "app",
+                table: "employess",
+                column: "company_id",
+                principalSchema: "app",
+                principalTable: "companies",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_employess_companies_company_id",
+                schema: "app",
+                table: "employess");
+
+            migrationBuilder.RenameColumn(
+                name: "company_id",
+                schema: "app",
+                table: "employess",
+                newName: "CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_employess_company_id",
+                schema: "app",
+                table: "employess",
+                newName: "IX_employess_CompanyId");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                schema: "app",
+                table: "companies",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_companies_name",
+                schema: "app",
+                table: "companies",
+                newName: "IX_companies_Name");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_employess_companies_CompanyId",
+                schema: "app",
+                table: "employess",
+                column: "CompanyId",
+                principalSchema: "app",
+                principalTable: "companies",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
