e6df202f65e0219fce3cc4cece09c27aa76e04c0
Сергей Лысков <sergkosya@mail.ru>
2023-04-20 10:40:18 +0300

    добавлен инициализатор конфигурации приложения

---
+++ AppExample/Inizializers/ConfigInitializer.cs
@@ -0,0 +1,38 @@
+﻿using AppExample.Configuration;
+
+using Microsoft.Extensions.Configuration;
+
+namespace AppExample.Inizializers;
+
+public class ConfigInitializer
+{
+    private readonly string[] _argsArray;
+
+    public ConfigInitializer()
+    {
+        _argsArray = new[] { "q", "d", "p" };
+    }
+
+    public (IConfiguration congig, string environment) GetConfiguration()
+    {
+        var input = string.Empty;
+        dynamic config = null!;
+
+        do
+        {
+            Console.WriteLine($"Введите:{Environment.NewLine}" +
+                    $"\"d\" для запуска конфигурации разработки{Environment.NewLine}" +
+                    $"\"p\" для запуска конфигурации релиза{Environment.NewLine}" +
+                    $"\"q\" для выхода из приложения");
+
+            input = Console.ReadLine();
+            config = Config.GetConfiguration(input);
+
+            if (config is null)
+                return (null, null)!;
+
+        } while (!_argsArray.Contains(input));
+
+        return config;
+    }
+}
