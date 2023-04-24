4d2cfe809481d0e3e9652005decfbf54f8062c0a
Сергей Лысков <sergkosya@mail.ru>
2023-04-24 21:40:59 +0300

    fix генерации id в БД

---
+++ AppExample.DAL/Migrations/20230424172227_UpdateIdDefaultValueSql.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppExample.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIdDefaultValueSql : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "app",
+                table: "employess",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "app",
+                table: "companies",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "app",
+                table: "employess",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "app",
+                table: "companies",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+        }
+    }
+}
